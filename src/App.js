f3de5895f01eb33462e8c246129749780ebb1a50
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-04 09:31:36 +0600

    tailwind working

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ function App() {
         <h1 className="text-3xl text-blue-600 font-bold underline">
           Hello world!
         </h1>
+
+        <button class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Save changes
+        </button>
       </header>
     </div>
   );
