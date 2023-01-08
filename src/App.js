4b9ac858843eaf944aa1c9ad026dbba813b26fcc
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-08 23:53:35 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <h1 className="text-3xl text-blue-600 font-bold underline">
-          Hello world!
-        </h1>
-
-        <button class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-          Save changes
-        </button>
-      </header>
+    <div>
+      <RouterProvider router={routes} />
     </div>
   );
 }
