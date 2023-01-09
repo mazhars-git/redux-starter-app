8836a8cc6cea79dd39767ef0ea8f52fbeba8a879
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-09 09:12:34 +0600

    color update

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,8 +5,8 @@ import { IoIosListBox } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <nav className='h-14 bg-indigo-200 rounded-full m-2 max-w-7xl mx-auto px-5'>
-      <ul className='h-full  mx-auto flex justify-between items-center gap-3 font-semibold text-indigo-900'>
+    <nav className='h-14 bg-orange-300 rounded-full m-2 max-w-7xl mx-auto px-5'>
+      <ul className='h-full  mx-auto flex justify-between items-center gap-3 font-semibold text-blue-900'>
         <h1 className='flex-1'>Moon Tech</h1>
         <li>
           <Link to='/'>Home</Link>
@@ -17,12 +17,12 @@ const Navbar = () => {
         <li>
           <Link to='/about'>About</Link>
         </li>
-        <li title='Wishlist' className='bg-indigo-500 p-2 rounded-full'>
+        <li title='Wishlist' className='bg-blue-900 p-2 rounded-full'>
           <Link to='/'>
             <IoIosListBox className='text-white' />
           </Link>
         </li>
-        <li title='cart' className='bg-indigo-500 p-2 rounded-full'>
+        <li title='cart' className='bg-blue-900 p-2 rounded-full'>
           <Link to='/cart'>
             <BsFillCartFill className='text-white ' />
           </Link>
