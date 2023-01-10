87d29105292d906b8f3f8691716cae7ab08b3520
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-10 09:41:54 +0600

    add to cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCreators/productActions";
+
 
 const ProductCard = ({ product }) => {
+
+  const dispatch = useDispatch();
+
   return (
     <div
       className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
@@ -20,9 +26,10 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+        <button onClick={() => dispatch(addToCart(product))} className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
+
         <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
