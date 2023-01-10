0163115419baecb44a1451e0e2072478aece84a6
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-10 08:20:55 +0600

    redux working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 import {Provider} from "react-redux";
-import {store} from "./redux/store"
+import store from "./redux/store"
 
 function App() {
   return (
