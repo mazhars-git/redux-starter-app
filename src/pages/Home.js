480123cc47a8e00fa508f78b87842a0952a8a653
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-11 21:36:43 +0600

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,13 +11,29 @@ const Home = () => {
     .then((data) => setProducts(data));
   },[])
 
-  const state = useSelector((state) => state);
+  const activeClass = "text-white  bg-indigo-500 border-white";
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {
-        products.map(product => <ProductCard key={product._id} product={product} />)
-      }
+    <div className='max-w-7xl gap-14 mx-auto my-10'>
+      <div className='mb-10 flex justify-end gap-5'>
+          <button
+            className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          >
+            In Stock
+          </button>
+          <button className={`border px-3 py-2 rounded-full font-semibold`}>
+            AMD
+          </button>
+          <button className={`border px-3 py-2 rounded-full font-semibold`}>
+            Intel
+          </button>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+          {
+            products.map(product => <ProductCard key={product._id} product={product} />)
+          }
+        </div>
+        
     </div>
   );
 };
