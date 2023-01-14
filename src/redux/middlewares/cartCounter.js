b06259b68f8ec7e13c128dd33ba496087feac19e
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-14 11:30:45 +0600

    update cart position

--- src/redux/middlewares/cartCounter.js
+++ src/redux/middlewares/cartCounter.js
@@ -1,4 +1,18 @@
+import { ADD_TO_CART } from './../actionTypes/actionTypes';
+
 const cartCounter = (store) => (next) => (action) => {
+    const state = store.getState();
+    const cart = state.product.cart;
+
+    if(action.type === ADD_TO_CART){
+        const newAction = {
+            ...action,
+            payload: {...action.payload, cartPosition: cart.length}
+        }
+
+        return newAction;
+    }
+
     return next(action);
 };
 
